c02ea1e293bd568e1238fd0378c2c7e79d06e1a8
BHUPALAVISHNUK <62935134+BHUPALAVISHNUK@users.noreply.github.com>
2026-09-12 19:37:30 +0530

    Add files via upload
    day01-firstprogram

---
+++ firstprogram.js
@@ -0,0 +1,12 @@
+var value
+var value1 =[]
+var value2 = null //null comes under object
+let value3 = {}
+
+console.log(typeof"Hello,world")
+console.log(786)
+console.log(typeof true)
+console.log (value)
+console.log(value1)
+console.log (typeof value2)
+console.log (typeof value3)
