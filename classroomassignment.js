68dd37bc220b34cc7ad0d4fbe936f0f007abf63f
BHUPALAVISHNUK <62935134+BHUPALAVISHNUK@users.noreply.github.com>
2026-09-12 21:20:54 +0530

    Add files via upload

---
+++ classroomassignment.js
@@ -0,0 +1,18 @@
+// const browserversion = "edge"
+// function getbrowserversion(){
+//     if (browser === "chrome") {
+//         var browser = "chrome";      
+//     }
+//     console.log(browserversion);
+// }
+// getbrowserversion();
+
+const browserversion = 'chrome'
+function getBrowserVersion(){
+    if (browserversion === "chrome") {
+        let browserversion = "firefore"
+        
+    }
+    console.log(browserversion)
+}
+getBrowserVersion();
